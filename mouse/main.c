6697c66e3a33acaaacfa29c0076333ac4f31e8d8
thftgr <44352170+thftgr@users.noreply.github.com>
2020-03-24 17:52:30 +0900

    .

--- mouse/main.c
+++ mouse/main.c
@@ -1,25 +1,36 @@
-#include <Windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <Windows.h>
+
+#define FILE_NAME "tmp.txt"
 
 
-
-
+void fileopen(FILE* file)
+{
+	
+	if (file == NULL)
+	{
+		printf_s("file open fall \n");
+		return 0;
+	}
+}
 
 
 int main()
 {
 
 	POINT p;
-	int* xy[2] = {0,0};
+	FILE* fp;
 
-	int *buf[100];
-	
-	int k = 0;
-	double StartTime = 0, StopTime = 0;
-	int check = 0;
-	
+	int* xy[2] = { 0,0 };
+	int StartTime = 0, StopTime = 0;
+	int check = 0, count = 0;
+	int* readbuffer[3] = { 0,0,0 };
+
+
+	remove(FILE_NAME);
+	//if (remove(FILE_NAME) != 0) return 0;
 
 	while (1)
 	{
@@ -29,39 +40,38 @@ int main()
 			check = 0;
 		}
 
-		GetCursorPos(&p);
-		if ((xy[0] != p.x) | (xy[1] != p.y))
+		GetCursorPos(&p); //커서 좌표 받아오기
+		if ((xy[0] != p.x) | (xy[1] != p.y)) //좌표의 변화가 있는경우만 
 		{
 			StopTime = clock();
 			check = 1;
 			xy[0] = p.x;
 			xy[1] = p.y;
-			int hz;
-			if ((StopTime - StartTime) == 0)
+
+			printf_s("X= %d, Y= %d : %dms = ", (int)xy[0], (int)xy[1], (StopTime - StartTime));
+
+			fopen_s(&fp, FILE_NAME, "a+");
+			fileopen(fp);
+			fprintf_s(fp, "%d %d %d\n", (int)xy[0], (int)xy[1], (StopTime - StartTime));
+			fclose(fp);
+
+
+			fopen_s(&fp, FILE_NAME, "a+");
+			fileopen(fp);
+
+			for (int i = -1; i < count; i++)
 			{
-				hz = 1000;
-			}
-			else
-			{
-				hz = (1000 / (StopTime - StartTime));
+				fscanf_s(fp, "%d %d %d", &readbuffer[0], &readbuffer[1], &readbuffer[2]);
 			}
 
-			buf[k] = p.x;
-			buf[++k] = p.y;
-			printf("X = %d, Y= %d : %dHz\n", (int)xy[0], (int)xy[1], hz);
+			printf_s("file read:%d,%d\n", readbuffer[0], readbuffer[1]);
+			fclose(fp);
+			count++;
 
-			k++;
-			if (k > 100)
-			{
-				break;
-			}
 		}
+
 	}
 
-	for (int i = 0; i < 100; i += 2)
-	{
-		printf("Log : X = %d, Y= %d\n", (int)buf[i], (int)buf[i+1]);
-	}
-
+	fclose(fp);
 	return 0;
 }
